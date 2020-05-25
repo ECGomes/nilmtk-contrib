8ba6564fde8ae009ee623206b5e67f8e625b09ce
ECGomes <camara.edu@gmail.com>
2020-05-25 02:15:07 +0100

    Added the quantile loss Added model definition

--- disaggregate/pbsingle.py
+++ disaggregate/pbsingle.py
@@ -4,22 +4,24 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+import tensorflow._api.v1.keras.backend as K
 
 
 class PB_Single(object):
-    """Provides a common interface to all disaggregation classes.
 
-    See https://github.com/nilmtk/nilmtk/issues/755 for discussion
+    def __init__(self, params):
+        """
+        Parameters to be specified for the model
+        """
 
-    Attributes
-    ----------
-    model :
-        Each subclass should internally store models learned from training.
+        self.MODEL_NAME = 'PB_Single'
+        self.window_size = params.get('window_size', 20)
+        self.n_epochs = params.get('n_epochs', 10)
+        self.batch_size = params.get('batch_size', 512)
+        self.pb_value = params.get('pb_value', 0.5)
+        self.n_features = params.get('n_features', 1)
 
-    MODEL_NAME : string
-        A short name for this type of model.
-        e.g. 'CO' for combinatorial optimisation.
-    """
+
 
     def partial_fit(self, train_mains, train_appliances, **load_kwargs):
         """ Trains the model given a metergroup containing appliance meters
@@ -31,7 +33,15 @@ class PB_Single(object):
                         the same pd.DatetimeIndex as index as train_main and
                         the same 1 or more power columns as train_main
         """
+
+        #TODO
+        # 1 - Call preprocessing
+        # 2 - Build model -> Kind of done
+        # 3 - Train the model
+
+
         raise NotImplementedError()
+
 
     def disaggregate_chunk(self, test_mains):
         """Passes each chunk from mains generator to disaggregate_chunk()
@@ -40,6 +50,7 @@ class PB_Single(object):
         test_mains : list of pd.DataFrames
         """
         raise NotImplementedError()
+
 
     def call_preprocessing(self, train_mains, train_appliances):
         """Calls the preprocessing functions of this algorithm and returns the
@@ -52,7 +63,54 @@ class PB_Single(object):
                     the same pd.DatetimeIndex as index as train_main and the
                     same 1 or more power columns as train_main
         """
+
+        #TODO
+        # 1 - Check for data formats
+        # 2 - Implement for one variable, expand as needed
+
+        print(train_mains)
+
         return train_mains, train_appliances
+
+
+    def pb_loss(self):
+        """Quantile loss function for model training"""
+
+        def custom_loss(y_true, y_pred):
+            tau = self.pb_value
+            err = y_true - y_pred
+
+            return K.mean(K.maximum(tau * err, (tau - 1) * err), axis=-1)
+
+        return custom_loss
+
+
+    def return_network(self):
+        """Returns the network for the PB-NILM single branch version"""
+
+        #TODO
+        # 1 - Check if loss function compiles
+        # 2 - Check if model itself compiles
+
+        inputs = tf.keras.layers.Input(shape=(self.window_size, self.n_features))
+
+        # Define the network
+        net = tf.keras.layers.Conv1D(128, kernel_size=5)(inputs)
+        net = tf.keras.layers.BatchNormalization()(net)
+        net = tf.keras.layers.Activation('relu')(net)
+        net = tf.keras.layers.MaxPooling1D()(net)
+
+        net = tf.keras.layers.GRU(256)(net)
+        net = tf.keras.layers.BatchNormalization()(net)
+        net = tf.keras.layers.Activation('relu')(net)
+        net = tf.keras.layers.Dense(1, activation='relu', name='Appliance')(net)
+
+        model = tf.keras.Model(inputs=inputs, outputs=net)
+
+        model.compile(optimizer='adam', loss=self.pb_loss)
+
+        return model
+
 
     def save_model(self, folder_name):
         """Passes each chunk from mains generator to disaggregate_chunk()
@@ -61,6 +119,7 @@ class PB_Single(object):
         test_mains : list of pd.DataFrames
         """
         raise NotImplementedError()
+
 
     def load_model(self, folder_name):
         """Passes each chunk from mains generator to disaggregate_chunk()
